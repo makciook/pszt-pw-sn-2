fe6661c15315e3c6ea89b3c2433c96a518e5e127
unknown44@gmail.com <unknown44@gmail.com@3dad1527-d51b-f067-cf21-fdb65fa6acae>
2013-04-04 18:37:33 +0000

    Komentarze, wyrzucenie kodu z konstruktora

--- project/src/NeuronNetwork.java
+++ project/src/NeuronNetwork.java
@@ -3,7 +3,6 @@
  * TODO:
  */
 
-import java.util.Vector;
 import java.util.Random;
 
 
@@ -13,7 +12,6 @@ public class NeuronNetwork {
     private Neuron[][] hiddenLayer;
     private Neuron[] outputLayer;
 
-
     private int neurons;
     private int layers_number;
 
@@ -21,13 +19,21 @@ public class NeuronNetwork {
     private final int OUTPUT_NEURONS = 2;
     private final int NEURONS_NUM = 20;
 
+    /*
     final double epsilon = 0.00000000001;
     final double learningRate = 0.9f;
     final double momentum = 0.7f;
+    */
 
     public NeuronNetwork(int lays_num) {
         layers_number = lays_num;
         neurons = NEURONS_NUM/lays_num;                        // ilość elementów w każdej z warstw ukrytych
+
+        createInputLayer();
+        createHiddenLayer(lays_num);
+        createOutputLayer();
+
+        /*
         inputLayer = new Neuron[2];                     // tworzenie warstwy wejściowej
         for(int i = 0; i < 2; ++i) {
             Neuron input = new Neuron();
@@ -70,19 +76,85 @@ public class NeuronNetwork {
                 con.setWeight(losuj.nextDouble()*2-1);
             }
         }
+        */
     }
 
+    /**
+     * Tworzenie warstwy wejściowej (2 neurony)
+     */
+    private void createInputLayer() {
+        inputLayer = new Neuron[2];
+        for(int i = 0; i < 2; ++i) {
+            Neuron input = new Neuron();
+            inputLayer[i] = input;
+        }
+    }
 
+    /**
+     * Tworzenie warstw ukrytych
+     * @param lays_num ilość warstw ukrytych do utworzenia
+     */
+    private void createHiddenLayer(int lays_num) {
+        hiddenLayer =  new Neuron[lays_num][neurons];                     // tworzenie lays_num warstw ukrytych
+        Neuron[] prev_lay = inputLayer;
+        for(int i = 0; i < lays_num; ++i) {
+            for(int j = 0; j < neurons; ++j) {
+                Neuron hidden = new Neuron();
+                hiddenLayer[i][j] = hidden;
+                hidden.addConnections(prev_lay);                // tworzymy połączenia z neuronami w poprzedniej warstwie
+            }
+            prev_lay = hiddenLayer[i];
+        }
 
+        // inicjalizacja wag losowymi wartościami
+        Random losuj = new Random();
+        for(int i = 0; i < lays_num; ++i) {
+            for(Neuron neuron : hiddenLayer[i]) {
+                Synaps[] cons = neuron.getConnections();
+                for(Synaps con : cons) {
+                    con.setWeight(losuj.nextDouble()*2-1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Tworzenie warstwy wyjściowej
+     */
+    private void createOutputLayer() {
+        Neuron[] prev_lay = hiddenLayer[layers_number-1];
+        outputLayer = new Neuron[OUTPUT_NEURONS];                    // tworzenie warstwy wyjściowej
+        for(int i = 0; i < OUTPUT_NEURONS; ++i) {
+            Neuron output = new Neuron();
+            outputLayer[i] = output;
+            output.addConnections(prev_lay);
+        }
+
+        // czytaj komentarz wyżej
+        Random losuj = new Random();
+        for(Neuron neuron : outputLayer) {
+            Synaps[] cons = neuron.getConnections();
+            for(Synaps con : cons) {
+                con.setWeight(losuj.nextDouble()*2-1);
+            }
+        }
+    }
+
+    /**
+     * Publiczna metoda uczenia sieci rozpoznawania punktu
+     * @param x współrzędna x punktu na płaszczyźnie
+     * @param y współrzędna y punktu na płaszczyźnie
+     * @param expected wartość oczekiwana (nauczona)
+     */
     public void learn(double x, double y, double expected[]) {
-        setInput(x,y);                                          // ustarwienie danych wejściowych
-        calculate();                                            // wykonanie obliczeń przez sieć
+        setInput(x,y);            // ustawienie danych wejściowych
+        calculate();              // wykonanie obliczeń przez sieć
 
         System.out.println("x: " + x + " y " + y);
         System.out.println("Oczekiwane: " + expected[0] + " " + expected[1]);
         System.out.println("Wynik " + outputLayer[0].getValue() + " "  + outputLayer[1].getValue());
 
-        applyBackpropagation(expected);
+        applyBackPropagation(expected);  // zastosowanie propagacji wstecznej
     }
 
     private void setInput(double x, double y) {
@@ -115,7 +187,11 @@ public class NeuronNetwork {
         return result;
     }
 
-    private void applyBackpropagation(double expected[]) {
+    /**
+     * Algorytm wstecznej propagacji dla podanego parametru oczekiwanego
+     * @param expected wartość parametru oczekiwanego
+     */
+    private void applyBackPropagation(double expected[]) {
 
         for(int i = 0; i < OUTPUT_NEURONS; ++i) {
             Neuron n = outputLayer[i];
@@ -160,7 +236,7 @@ public class NeuronNetwork {
         changeHiddenLayersWeights();*/
     }
 
-    private void calcOutputLayerError(double expected[]) {
+    /*private void calcOutputLayerError(double expected[]) {
         double out;
         double delta;
         for(int i = 0; i < OUTPUT_NEURONS; ++i) {
@@ -212,5 +288,6 @@ public class NeuronNetwork {
             }
         }
     }
+    */
 
 }
